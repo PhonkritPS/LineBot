63a4eeff89d8119620cbd9d725ad735186c1fb24
PhonkritPS <k.phonkrit@gmail.com>
2021-02-09 18:37:06 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "FixitCenter/"; //enter your appid
-     $KEY = "0ZcxhUOlSSrdtMt"; //enter your key
-    $SECRET = "JS9MqcQ2QyuujStrl7Y7Nqim2"; //enter your secret
+       $APPID= "445ebdc0-0985-4c85-9bd3-55c0a5001ce0/"; //enter your appid
+     $KEY = "Y6hfnAKtT9RGg2pe6LzRuWyJowzb9S1U"; //enter your key
+    $SECRET = "wh5u_*G#0RfZu*uCG2vIl~37_9LeA(m6"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
@@ -45,7 +45,7 @@
       echo $response . "\r\n";
     return $response;
 }
-// $Topic = "NodeMCU1";
+// $Topic = "Linebot";
  //$lineMsg = "CHECK";
  //getMqttfromlineMsg($Topic,$lineMsg);
 ?>
